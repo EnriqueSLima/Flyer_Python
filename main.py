d5b340b2c23664a5f0e660434d735bceee8199ea
EnriqueSLima <souza.enrique7@gmail.com>
2023-07-01 17:53:50 -0300

    Build all inside main app

--- main.py
+++ main.py
@@ -9,11 +9,42 @@ Window.size = (380, 700)
 class FlyerManager(ScreenManager):
     pass
 class SignIn(Screen):
+    def __init__(self, **kw):
+        super(SignIn, self).__init__(**kw)
     pass
+
 class SignUp(Screen):
+    def __init__(self, **kw):
+        super(SignUp, self).__init__(**kw)
+    def on_pre_enter(self):
+        Window.bind(on_keyboard = self.back)
+    def back(self, window, key, *args):
+        if key == 27:
+            App.get_running_app().root.current = 'sign_in_screen'
+            return True
+    def on_pre_leave(self, *args):
+        Window.unbind(on_keyboard = self.back)
     pass
 
+class Validation(Screen):
+    def __init__(self, **kw):
+        super(Validation, self).__init__(**kw)
 
+    def on_pre_enter(self):
+        Window.bind(on_keyboard = self.back)
+
+    def back(self, window, key, *args):
+        if key == 27:
+            App.get_running_app().root.current = 'sign_up_screen'
+            return True
+    def on_pre_leave(self, *args):
+        Window.unbind(on_keyboard = self.back)
+    pass
+
+class FlyerIndex(Screen):
+    def __init__(self, **kw):
+        super(FlyerIndex, self).__init__(**kw)
+    pass
 
 # Runs Application
 class Flyer(App):
