1fd4759b995f176a72403155251fb6c93d47d542
EnriqueSLima <souza.enrique7@gmail.com>
2023-07-06 12:38:28 -0300

    Deletando telas antigas, otimizando codigo

--- main.py
+++ main.py
@@ -1,64 +1,64 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy_garden.mapview import MapMarkerPopup
+from kivy.uix.label import Label
+from kivy_garden.mapview import MapView
 from kivy.core.window import Window
 
 Window.size = (380, 700)
 
-
+#   Main Screen Manager
 class FlyerManager(ScreenManager):
     pass
+
+#   Sign-in Screen
 class SignIn(Screen):
     def __init__(self, **kw):
-        super(SignIn, self).__init__(**kw)
-    pass
+        super().__init__(**kw)
 
+#   Sign-up Screen
 class SignUp(Screen):
     def __init__(self, **kw):
-        super(SignUp, self).__init__(**kw)
-    def on_pre_enter(self):
+        super().__init__(**kw)
+
+    def on_pre_enter(self): # bind ESC for previous screen
         Window.bind(on_keyboard = self.back)
+
     def back(self, window, key, *args):
-        if key == 27:
+        if key == 27: # When ESC is pressed
             App.get_running_app().root.current = 'sign_in_screen'
             return True
-    def on_pre_leave(self, *args):
+    def on_pre_leave(self, *args): # unbind ESC for previous screen
         Window.unbind(on_keyboard = self.back)
-    pass
 
+#   Validation Screen 
 class Validation(Screen):
     def __init__(self, **kw):
         super(Validation, self).__init__(**kw)
 
-    def on_pre_enter(self):
+    def on_pre_enter(self): # bind ESC for previous screen
         Window.bind(on_keyboard = self.back)
 
     def back(self, window, key, *args):
-        if key == 27:
+        if key == 27: # When ESC is pressed
             App.get_running_app().root.current = 'sign_up_screen'
             return True
-    def on_pre_leave(self, *args):
+    def on_pre_leave(self, *args): # unbind ESC for previous screen
         Window.unbind(on_keyboard = self.back)
-    pass
 
-class FlyerList(Screen):
+class FlyerMap(MapView):
     pass
-class FlyerSearch(Screen):
-    pass
-class FlyerHome(Screen):
-    pass
-class FlyerPromo(Screen):
-    pass
-class FlyerFav(Screen):
-    pass
-class FlyerSettings(Screen):
-    pass
+#   Main Screen
 
 class FlyerIndex(Screen):
     def __init__(self, **kw):
-        super(FlyerIndex, self).__init__(**kw)
-    pass
+        super().__init__(**kw)
+
+    def changeLayout(self):
+        self.ids.main.add_widget(FlyerMap)
+        print('TESTE')
+        pass
+        
 
 # Runs Application
 class Flyer(App):
